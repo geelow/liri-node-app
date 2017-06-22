dc0f6943b191e358ca99f79ed4b42a5f27c5aca9
Jamie Gelo <jamie.gelo@Jamie-Gelos-MBPR.local>
2017-06-21 21:19:37 -0500

    starting the liri.js code

---
+++ liri.js
@@ -0,0 +1,11 @@
+var twitterKeys = require("./keys.js");
+var fs = require("fs");
+
+//need to add function reference to update text file with function. WriteFile creates a new file
+//fs.writeFile("log.txt", /*______*/, functio(err) {
+//    if (err) {
+//        return console.log(err);
+//    }
+//});
+
+
