794f2ecd03aabd4ff04210e9fde4be9f2611118c
Jamie Gelo <jamie.gelo@Jamie-Gelos-MBPR.local>
2017-06-21 20:46:57 -0500

    intial file creation and set up

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<kB6Y3GliX8n78RKDuoXvhZKiw>',
+  consumer_secret: '<gvqLQlJmDQEpr5HBIbog6RutJS5jfCzfHGnFNHvo8coHjuIxjC>',
+  access_token_key: '<17330078-P1XULSdIAPE9HX2mhdT50o21WrHigQdQyoxbdVKIM>',
+  access_token_secret: '<Hm4n39AUETSxYsvbXFH2X2H1t2pgoGZ7w1RgJAYkd0YM7>',
+}
